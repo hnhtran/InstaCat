83d6c9bd67c544629eed43572be4d9fa32ef1552
hnhtran <tran_eetas@yahoo.com>
2022-07-03 20:35:49 +1000

    update users.js controller login

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,5 +1,24 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+
 module.exports = {
-    create
+    create,
+    login
+}
+
+// login
+async function login(req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        // check user and password exist or not
+        if (!user) throw new Error()
+        const match = await bcrypt.compare(req.body.password, user.password)
+        if (!match) throw new Error()
+        res.json( createJWT(user) )
+    } catch(error) {
+        res.status(400).json(error)
+    }
 }
 
 //  create user from model
