b29f5b797ccb955409ce3878168291780f906d5b
hnhtran <tran_eetas@yahoo.com>
2022-07-03 20:24:29 +1000

    update users.js controller with createjws helper function

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -6,10 +6,20 @@ module.exports = {
 async function create(req, res) {
     try {
         const user = await User.create(req.body)
-        const token = createdJWT(user)
-        
+        const token = createJWT(user)
+
         res.json(token)
     } catch(error) {
         res.status(400).json(error)
     }
+}
+
+// ================== Helper Function ==================
+function createJWT(user) {
+    return jwt.sign(
+        // data payload
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h'}
+    )
 }
