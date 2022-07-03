0b452c29c4380adeea7ec9801a04eff1933f6e5f
hnhtran <tran_eetas@yahoo.com>
2022-07-03 15:33:06 +1000

    users-api update for login

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -6,3 +6,7 @@ const BASE_URL = '/api/users'
 export async function signUp(userData) {
     return sendRequest(BASE_URL, 'POST', userData)
 }
+
+export async function logIn(credentials) {
+    return sendRequest(`${BASE_URL}/login`, 'POST', credentials)
+}
