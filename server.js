b18c575f7e45db84e23886cbed8f024f17f86e40
hnhtran <tran_eetas@yahoo.com>
2022-07-03 16:04:50 +1000

    mount /api/user to user routes

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 // Put API routes here, before the "catch all" route
-
+// any routes leads to user now is shorten by '/' only in users router
+app.use('/api/users', require('./routes/api/users'))
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
 app.get('/*', (req, res) => {
