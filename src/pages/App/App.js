79f242e78f350036d988f164b28f79792a1ebe7e
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-05 13:49:28 -0400

    added state to APP.js and imports

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,6 +2,9 @@ import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 // import Test from './Test';
 import AuthPage from '../AuthPage/AuthPage';
+import { getUser } from '../../utilities/users-service';
+import { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   const [user, setUser] = useState(getUser())
