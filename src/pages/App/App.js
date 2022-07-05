8e9b06c82b0a3450e2985ad780409374ee262cf6
Jason Lull <jlull7774@gmail.com>
2022-07-05 15:57:13 -0400

    made changes to nav bar. got icons working. changed home page functionality

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,21 +1,21 @@
-import NavBar from '../../components/NavBar/NavBar';
-import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
-import { useState } from "react";
-import { Routes, Route } from 'react-router-dom';
-import { getUser } from '../../utilities/users-service';
+import NavBar from "../../components/NavBar/NavBar"
+import "./App.css"
+import AuthPage from "../AuthPage/AuthPage"
+import { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import { getUser } from "../../utilities/users-service"
+import HomePage from "../HomePage/HomePage"
 
 export default function App() {
+	// const [user, setUser] = useState(getUser())
 
-  const [user, setUser] = useState(getUser())
-  
-  return (
-    <div className="App">
-    <NavBar user={user} setUser={setUser} />
-      App
-      {/* <Test /> */}
-      <AuthPage />
-    </div>
-  );
+	return (
+		<div className='App'>
+			{/* <NavBar user={user} setUser={setUser} /> */}
+			App
+			{/* <Test /> */}
+			{/* <AuthPage /> */}
+			<HomePage />
+		</div>
+	)
 }
-
