d02a813fe923b81ede0af687ae4063ca82e2e9c1
hnhtran <tran_eetas@yahoo.com>
2022-07-05 23:32:25 +1000

    update post model

--- models/post.js
+++ models/post.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema
 const postSchema = mongoose.Schema({
     description: String,
     selectedFile: String
+}, {
+    timestamps: true
 })
 
 module.exports = mongoose.model('Post', postSchema)
