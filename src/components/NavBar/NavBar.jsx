2b93642749c11f599fa9613050b6c505941a8098
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-05 13:45:51 -0400

    Added user state, Update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,17 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
 </style>
 // Using the import below, we can call any exported function using: userService.someMethod()
 // import * as userService from '../../utilities/users-service';
 export default function NavBar() {
-  //   function handleLogOut() {
-  //     // Delegate to the users-service
-  //     userService.logOut();
-  //     // Update state will also cause a re-render
-  //     setUser(null);
-  //   }
+    function handleLogOut() {
+      // Delegate to the users-service
+      userService.logOut();
+      // Update state will also cause a re-render
+      setUser(null);
+    }
   
   return (
     <div class="header">
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,9 +4,12 @@ import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 
 export default function App() {
+
+  const [user, setUser] = useState(getUser())
+  
   return (
     <div className="App">
-    <NavBar />
+    <NavBar user={user} setUser={setUser} />
       App
       {/* <Test /> */}
       <AuthPage />
