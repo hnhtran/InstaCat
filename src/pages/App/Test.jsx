83e0d4c39ad78f76feec5874f165fc98db74f276
hnhtran <tran_eetas@yahoo.com>
2022-07-06 00:48:23 +1000

    corporate cat facts into html body

--- src/pages/App/Test.jsx
+++ src/pages/App/Test.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Test = () => {
+    const [fact, setFact] = useState('')
     const searchCatFacts = async (fact) => {
         const response = await fetch(`https://catfact.ninja/fact?max_length=${fact}`)
         const data = await response.json()
-        console.log(data)
+        console.log(data.fact)
+        setFact(data.fact)
     }
-    searchCatFacts()
 
   return (
-    <div>Test</div>
+    <div>
+        {/* {searchCatFacts()} */}
+    <button onClick={() => searchCatFacts()}>Click for Cat Facts</button>
+    <h1>
+        {fact}
+    </h1>
+    </div>
   )
 }
 
