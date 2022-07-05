716c207b99cd9afd95a512d4137f2a04db19291b
Jason Lull <jlull7774@gmail.com>
2022-07-05 16:23:50 -0400

    final commit for the day

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,33 +1,34 @@
 import "./Post.css"
-import MoreVertIcon from "@mui/icons-material/MoreVert"
-import FavoriteIcon from "@mui/icons-material/Favorite"
-export default function Post({ user }) {
+import { MoreVert } from "@mui/icons-material/"
+
+import { Favorite } from "@mui/icons-material/"
+export default function Post() {
 	return (
 		<>
 			<div className='post'>
 				<div className='postWrapper'>
 					<div className='postTop'>
 						<div className='postTopLeft'>
-							<span className='postUsername'>{user.name}</span>
+							<span className='postUsername'>{}</span>
 							<span className='postDate'>Less than an hour ago</span>
 						</div>
 						<div className='postTopRight'>
-							<MoreVertIcon />
+							<MoreVert />
 						</div>
 					</div>
 
 					<div className='postCenter'>
 						<span className='postText'>Testing!</span>
-                        <img src="" alt="" />
+						<img src='' alt='' />
 					</div>
 					<div className='postBottom'>
 						<div className='postBottomLeft'>
-							<FavoriteIcon />
-                            <span className='postFavorites'>9 likes</span>
+							<Favorite />
+							<span className='postFavorites'>9 likes</span>
 						</div>
 						<div className='postBottomRight'>
-                            <span className="postComment">2 comments</span>
-                        </div>
+							<span className='postComment'>2 comments</span>
+						</div>
 					</div>
 				</div>
 			</div>
