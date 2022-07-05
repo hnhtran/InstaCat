e37e7e49bed855e44003e4250174c015851953d4
Jason Lull <jlull7774@gmail.com>
2022-07-05 13:48:17 -0400

    added blank img for testing

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -18,6 +18,7 @@ export default function Post({ user }) {
 
 					<div className='postCenter'>
 						<span className='postText'>Testing!</span>
+                        <img src="" alt="" />
 					</div>
 					<div className='postBottom'>
 						<div className='postBottomLeft'>
