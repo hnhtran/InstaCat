36943c8eefc9635d760926c7f59e790f7ddff4bf
Jason Lull <jlull7774@gmail.com>
2022-07-05 13:40:03 -0400

    added timestamps and styling to posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,17 +1,18 @@
 import "./Post.css"
-
-export default function Post({user}) {
+import MoreVertIcon from "@mui/icons-material/MoreVert"
+export default function Post({ user }) {
 	return (
 		<>
 			<div className='post'>
 				<div className='postWrapper'>
 					<div className='postTop'>
 						<div className='postTopLeft'>
-                            <span className="postUsername">
-                                { user.name }
-                            </span>
-                        </div>
-						<div className='postTopRight'></div>
+							<span className='postUsername'>{user.name}</span>
+							<span className='postDate'>Less than an hour ago</span>
+						</div>
+						<div className='postTopRight'>
+							<MoreVertIcon />
+						</div>
 					</div>
 
 					<div className='postCenter'></div>
