3689fc4d01e8870c1c2e43b89abadd25fe739a86
hnhtran <tran_eetas@yahoo.com>
2022-07-06 05:52:08 +1000

    signup works

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,9 @@
-import './NavBar.css';
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap’);
-</style>
-// Using the import below, we can call any exported function using: userService.someMethod()
-export default function NavBar({ user, setUser }) {
-    function handleLogOut() {
-      // Delegate to the users-service
-      userService.logOut();
-      // Update state will also cause a re-render
-      setUser(null);
-    }
+import React from 'react'
+
+const NavBar = () => {
   return (
-    <div class='header'>
-  <Link to="" className='logo'>InstaCat</Link>
-  <div className='header-right'>
-    <a class='active' href='#home'><img src='images/homeicon.png' className='icon' alt='' /></a>
-    <a href='#contact'><img src='images/newposticon.png' className='icon' alt='' /></a>
-    <a href='#about'><img src='../../images/usericon.png' className='icon' alt='' /></a>
-  </div>
-</div>
-  );
+    <div>NavBar</div>
+  )
 }
 
+export default NavBar
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,33 +1,26 @@
-import NavBar from '../../components/NavBar/NavBar';
-import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
+import NavBar from "../../components/NavBar/NavBar";
+import "./App.css";
+import AuthPage from "../AuthPage/AuthPage";
 import { useState } from "react";
-import { Routes, Route } from 'react-router-dom';
-import { getUser } from '../../utilities/users-service';
-import HomePage from '../HomePage/HomePage'
- 
+import { Routes, Route } from "react-router-dom";
+import { getUser } from "../../utilities/users-service";
+import HomePage from "../HomePage/HomePage";
 
 export default function App() {
+  const [user, setUser] = useState(getUser());
 
-  // const [user, setUser] = useState(getUser())
-  
   return (
     <div className="App">
-    {/* { user ?
-    <> */}
-    {/* <NavBar user={user} setUser={setUser} /> */}
-    {/* <NavBar />
-    <Routes>
-      <Route path="/home" element={<HomePage />} />
-      <Route path="" element={<UserProfilePage />}/>
-    </Routes> */}
-      App
-    {/* </> */}
-    {/* : */}
-      {/* <AuthPage setUser={setUser} /> */}
-      <AuthPage/>
-    {/* } */}
+      {
+        user ? (
+          <>
+            <NavBar user={user} setUser={setUser} />
+          </>
+        ) : (
+          <AuthPage setUser={setUser} />
+        )
+        // <AuthPage/>
+      }
     </div>
   );
 }
-
