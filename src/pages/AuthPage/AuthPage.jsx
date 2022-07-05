13c1ff23b68ce0d7c9ca5c8ba485acff3905c7eb
annakoanna <anna.231991k@gmail.com>
2022-07-05 16:33:11 -0700

    SignUp style css

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -8,11 +8,11 @@ export default function AuthPage({ setUser }) {
 
   return (
     <main className="AuthPage">
-      <div>
-    
-        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
-      </div>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+
+    <div className="signUpButton" onClick={() => setShowLogin(!showLogin)}>
+        {showLogin ? 'SIGN UP' : 'LOG IN'}
+    </div>
     </main>
   );
 }
