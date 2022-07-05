d89345b1e304bfba54cb5d1be174f8021ed10ce8
Jason Lull <jlull7774@gmail.com>
2022-07-05 13:32:14 -0400

    added props to function and more styling

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,19 @@
 import "./Post.css"
 
-export default function Post() {
+export default function Post({user}) {
 	return (
 		<>
 			<div className='post'>
 				<div className='postWrapper'>
-					<div className='postTop'></div>
+					<div className='postTop'>
+						<div className='postTopLeft'>
+                            <span className="postUsername">
+                                { user.name }
+                            </span>
+                        </div>
+						<div className='postTopRight'></div>
+					</div>
+
 					<div className='postCenter'></div>
 					<div className='postBottom'></div>
 				</div>
