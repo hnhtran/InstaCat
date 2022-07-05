000f1f565849deebdbdbd6fcdf650c9819800141
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-05 13:47:51 -0400

    User state added

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,9 +4,10 @@ import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 
 export default function App() {
+  const [user, setUser] = useState(getUser())
   return (
     <div className="App">
-    <NavBar />
+    <NavBar user={user} setUser={setUser}/>
       App
       {/* <Test /> */}
       <AuthPage />
