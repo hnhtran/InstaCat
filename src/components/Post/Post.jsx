5e88b5226e2c0d3525f09520fbbadad12789eb54
Jason Lull <jlull7774@gmail.com>
2022-07-05 13:29:12 -0400

    added post top, center, and bottom, for styling

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,15 @@
 import "./Post.css"
 
 export default function Post() {
-    return (
-        <>
-            <div>
-                test
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className='post'>
+				<div className='postWrapper'>
+					<div className='postTop'></div>
+					<div className='postCenter'></div>
+					<div className='postBottom'></div>
+				</div>
+			</div>
+		</>
+	)
 }
