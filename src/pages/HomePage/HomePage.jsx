0a671e4598f458ebc5df880de9cd55b214c169c9
Jason Lull <jlull7774@gmail.com>
2022-07-05 11:32:02 -0400

    imported components into home page jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,7 @@
-
+import NavBar from '../../components/NavBar/NavBar'
+import Sidebar from '../../components/SideBar/SideBar'
+import Feed from '../../components/Feed/Feed'
+import Footer from '../../components/Footer/Footer'
 
 export default function Home() {
 	return (
