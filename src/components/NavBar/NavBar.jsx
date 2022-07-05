1e9c1faaa3cfa59548df4b3b0df765efbf885303
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-05 13:18:38 -0400

    Navbar update not working

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,26 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-<style>
+import * as userService from '../../utilities/users-service';
+{/* <style>
 @import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
-</style>
+</style> */}
 // Using the import below, we can call any exported function using: userService.someMethod()
-// import * as userService from '../../utilities/users-service';
-export default function NavBar() {
-  //   function handleLogOut() {
-  //     // Delegate to the users-service
-  //     userService.logOut();
-  //     // Update state will also cause a re-render
-  //     setUser(null);
-  //   }
+export default function NavBar({user, setUser}) {
+    function handleLogOut() {
+      // Delegate to the users-service
+      userService.logOut();
+      // Update state will also cause a re-render
+      setUser(null);
+    }
   
   return (
-    <div class="header">
-  <a href="#default" class="logo">InstaCat</a>
-  <div class="header-right">
-    <a class="active" href="#home"><img src="images/homeicon.png" className="icon" alt="" /></a>
-    <a href="#contact"><img src="images/newposticon.png" className="icon" alt="" /></a>
-    <a href="#about"><img src="images/usericon.png" className='icon' alt="" /></a>
-  </div>
-</div>
+    <nav className="header">
+      {/* <Link to=""><img src="images/homeicon.png" className="icon" alt=""/></Link> */}
+      <Link to="">TEST</Link>
+      <Link to=""><img src="images/newposticon.png" className="icon" alt=""/></Link>
+      Welcome, {user.name}
+      <Link to=""><img src="images/usericon.png" className="icon" alt=""/></Link>
+      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+    </nav>
   );
 }
