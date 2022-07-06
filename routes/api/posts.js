a0c04ce1a1f09caa74a6ef3ff2c834164bb6654d
hnhtran <tran_eetas@yahoo.com>
2022-07-06 18:38:10 +1000

    posts routes update

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 const postsCtrl = require('../../controllers/api/posts')
 
-// GET /api/posts
-router.get('/', postsCtrl.index) // !need to define this
-// GET /api/posts/:id
+// api/posts/
+router.get('/', postsCtrl.createPost)
 router.get('/:id', postsCtrl.getPost)
+router.put('/:id', postsCtrl.updatePost)
+router.delete('/:id', postsCtrl.deletePost)
+router.post('/:id/like', postsCtrl.likePost)
+
+router.get('/', postsCtrl.index) // !need to define this
 
 module.exports = router
