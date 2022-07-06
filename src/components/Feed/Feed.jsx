8368733d5ae1854031b1465f6c9a864d75c4a342
Jason Lull <jlull7774@gmail.com>
2022-07-06 13:59:50 -0400

    changed styling to posts and new posts.

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,14 @@
+import { SevenKRounded } from "@mui/icons-material"
 import NewPost from "../NewPost/NewPost"
 import Posts from "../Posts/Posts"
+import SeededData from "../../SeededData"
 export default function Feed() {
 	return (
 		<>
 			<div className='feed'>
 				<div className='feedcontainer'>
 					<NewPost />
+
 					<Posts />
 				</div>
 			</div>
--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,38 +1,44 @@
-import { AddAPhoto } from "@mui/icons-material/";
-import "./NewPost.css";
-import FileBase64 from "react-file-base64";
-import { useState } from "react";
+import { AddAPhoto } from "@mui/icons-material/"
+import "./NewPost.css"
+import FileBase64 from "react-file-base64"
+import { useState } from "react"
 
 export default function NewPost() {
-  const [first, setFirst] = useState("");
-  return (
-    <>
-      <div className="newPosts">
-        <div className="newPostsContainer">
-          <div className="newPostsTop">
-            <img className="postsProfileimg" src={first} alt="Add Photo" />
-            <input placeholder="Show us your cats!" className="newPostInput" />
-          </div>
-          <hr className="newPostHr" />
-          <div className="newPostsBottom">
-            <div className="newPostsOptions">
-              <div className="option">
-                <AddAPhoto />
-                <span className="newPostsText">Photo</span>
-                <FileBase64
-                  type="file"
-                  multiple={false}
-                  onDone={({ base64 }) => setFirst(base64)}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-		{/* {first} */}
-		<img className="postsProfileimg" src={first} alt="Add Photo" height='50px'/>
-		</div>
-    </>
-  );
+	const [first, setFirst] = useState("")
+	return (
+		<>
+			<div className='newPosts'>
+				<div className='newPostsContainer'>
+					<div className='newPostsTop'>
+						<img className='postsProfileimg' src={first} alt='Add Photo' />
+						<input placeholder='Show us your cats!' className='newPostInput' />
+					</div>
+					<hr className='newPostHr' />
+					<div className='newPostsBottom'>
+						<div className='newPostsOptions'>
+							<div className='option'>
+								<a href=''></a>
+								<AddAPhoto className='icon' />
+								<span className='newPostsText'>Photo</span>
+								<FileBase64
+									type='file'
+									multiple={false}
+									onDone={({ base64 }) => setFirst(base64)}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				{/* {first} */}
+				<img
+					className='postsProfileimg'
+					src={first}
+					alt='Add Photo'
+					height='50px'
+				/>
+			</div>
+		</>
+	)
 }
