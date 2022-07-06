0d19712d4f7e6b1590b430fd29d4d71ebceee1ac
hnhtran <tran_eetas@yahoo.com>
2022-07-07 06:01:36 +1000

    posts index update

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,6 @@ router.put('/:id', postsCtrl.updatePost)
 router.delete('/:id', postsCtrl.deletePost)
 router.post('/:id/like', postsCtrl.likePost)
 
-router.get('/', postsCtrl.index) // !need to define this
+router.get('/', postsCtrl.getPosts) // !need to define this
 
 module.exports = router
