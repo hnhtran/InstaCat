8b7c0e9304ec6e6ebe2eedc0a81b4caeadae8a81
hnhtran <tran_eetas@yahoo.com>
2022-07-08 07:08:23 +1000

    revert commits

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,111 +1,94 @@
-import { AddAPhoto } from "@mui/icons-material/"
-import "./NewPost.css"
-import FileBase64 from "react-file-base64"
+import { AddAPhoto } from "@mui/icons-material/";
+import "./NewPost.css";
+import FileBase64 from "react-file-base64";
 import { useParams } from "react-router-dom"
-import { useState } from "react"
-import { createPost } from "../../utilities/userProfile-api"
+import { useState } from "react";
+import { createPost } from '../../utilities/userProfile-api'
 
-import React from "react"
-import { ReactDOM } from "react"
 
-export default function NewPost({ user, post, setPost }) {
-	let { userId } = useParams()
+export default function NewPost({user, post, setPost}) {
+  let { userId } = useParams()
 	console.log(user)
-	const [postData, setPostData] = useState({
-		userId: user._id,
-		userName: user.name,
-		likes: 0,
-		description: "",
-		image: "",
-	})
+  const [postData, setPostData] = useState({
+	userId: user._id,
+  userName: user.name,
+  likes: 0,
+    description: "",
+    image: "",
+  })
 
-	const handleChange = (event) => {
+  const handleChange = (event) => {
 		setPostData({
 			...postData,
 			[event.target.name]: event.target.value,
-		})
+		});
 	}
-	const [file, setFile] = useState(null)
-	// const handleImageChange = (event) => {
+	//   const handleImageChange = (event) => {
 	// 	setPostData({
 	// 		...postData,
 	// 		image: event.base64,
-	// 	})
+	// 	});
 	// }
 	const handleSubmit = async (e) => {
-		e.preventDefault()
+		e.preventDefault();
 		// console.log(postData)
 		const newPost = await createPost(postData, userId)
 		// console.log(post)
 		setPost(newPost)
 	}
 
-  // const [image, setImage] = useState(null);
-  // const [imageUrl, setImageUrl] = useState(null);
-  // const onImageChange = (e) => {
-  // 	if(e.target.files && e.target.files[0]){
-  // 		setImage(e.target.files[0]);
-  // 	}
-  // 	// setImage(e.target.files[0]);
-  // 	// setImageUrl(URL.createObjectURL(e.target.files[0]));
-  // 	setPostData({...postData, image: image})
-  // 	// console.log(imageUrl)
-  // 	// console.log(image)
-  // }
+	// const [image, setImage] = useState(null);
+	// const [imageUrl, setImageUrl] = useState(null);
+	// const onImageChange = (e) => {
+	// 	if(e.target.files && e.target.files[0]){
+	// 		setImage(e.target.files[0]);
+	// 	}
+	// 	// setImage(e.target.files[0]);
+	// 	// setImageUrl(URL.createObjectURL(e.target.files[0]));
+	// 	setPostData({...postData, image: image})
+	// 	// console.log(imageUrl)
+	// 	// console.log(image)
+	// }
 
-	return (
-		<>
-			<div className='newPosts' onSubmit={handleSubmit}>
-				<form autoComplete='off'>
-					<div className='newPostsContainer'>
-						<div className='newPostsTop'>
-							<img className='postsProfileimg' src='' alt='Add Photo' />
-							<input
-								placeholder='Show us your cats!'
-								className='newPostInput'
-								type='text'
-								name='description'
-								value={postData.description}
-								onChange={handleChange}
-								required
-							/>
-						</div>
-						<hr className='newPostHr' />
-						<form
-							className='newPostsBottom'
-							// onSubmit={submitHandler}
-						>
-							<div className='newPostsOptions'>
-								<label htmlFor='file' className='option'>
-									<AddAPhoto />
-									<span className='newPostsText'></span>
-									<input
-										style={{ display: "none" }}
-										type='file'
-										id='file'
-										accept='.png,.jpeg,.jpg'
-										onChange={(e) => setFile(e.target.files[0])}
-									/>
-									<FileBase64
-										className='chooseFile'
-										type='file'
-										multiple={false}
-										name='image'
-										onDone={({ base64 }) =>
-											setPostData({ ...postData, image: base64 })
-										}
-									/>
-									{/* <input type='file' onChange={onImageChange}/> */}
-								</label>
-							</div>
-							<button className='button' type='submit'>
-								Add post
-							</button>
-						</form>
-					</div>
-				</form>
-			</div>
-			<div></div>
-		</>
-	)
+  return (
+    <>
+      <div className="newPosts" onSubmit={handleSubmit}>
+        <form autoComplete="off">
+          <div className="newPostsContainer">
+            <div className="newPostsTop">
+              <img className="postsProfileimg" src='' alt="Add Photo" />
+              <input
+                placeholder="Show us your cats!"
+                className="newPostInput"
+				type='text'
+				name='description'
+				value={postData.description}
+				onChange={handleChange}
+				required
+              />
+            </div>
+            <hr className="newPostHr" />
+            <div className="newPostsBottom">
+              <div className="newPostsOptions">
+                <div className="option">
+                  <AddAPhoto />
+                  <span className="newPostsText">Photo</span>
+                  <FileBase64
+                    type="file"
+                    multiple={false}
+					name='image'
+                    onDone={({ base64 }) => setPostData({...postData, image: base64})}
+                  />
+				  {/* <input type='file' onChange={onImageChange}/> */}
+                </div>
+              </div>
+              <button type="submit">Add post</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div>
+      </div>
+    </>
+  );
 }
