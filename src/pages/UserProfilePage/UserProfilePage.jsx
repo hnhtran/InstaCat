9ca60e294f38c45fe69f02ed01feda1afbd09400
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-07-07 10:24:54 -0400

    Added settings icon, fixed icon page connections

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import NewPost from "../../components/NewPost/NewPost";
+// import NewPost from "../../components/NewPost/NewPost";
 
 const UserProfilePage = ({user, post, setPost}) => {
   return (
     <div>
-      <h1>UserProfilePage</h1>
-      <NewPost user={user} post={post} setPost={setPost}/>
+      <h1>User Profile Page</h1>
+      {/* <NewPost user={user} post={post} setPost={setPost}/> */}
     </div>
   );
 };
