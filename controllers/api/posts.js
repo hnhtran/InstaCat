ab5ac1f31d9dc1a2ab1b10389c764683f416a244
hnhtran <tran_eetas@yahoo.com>
2022-07-08 04:11:09 +1000

    posts controllers update

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -73,19 +73,19 @@ async function updatePost(req, res) {
 // delete a post
 async function deletePost(req, res) {
     console.log(req.body)
-    // const userId = req.user._id
-    // const postId = req.params.id
-    // try {
-    //     const post = await Post.findById(postId)
-    //     if (post.userId !== userId) {
-    //         res.status(401).json({ message: 'Unauthorized delete' })
-    //     } else {
-    //         await Post.findByIdAndDelete(postId)
-    //         res.json({ message: 'Post deleted' })
-    //     }
-    // } catch (err) {
-    //     res.json(err)
-    // }
+    const userId = req.user._id
+    const postId = req.params.id
+    try {
+        const post = await Post.findById(postId)
+        if (post.userId !== userId) {
+            res.status(401).json({ message: 'Unauthorized delete' })
+        } else {
+            await Post.findByIdAndDelete(postId)
+            res.json({ message: 'Post deleted' })
+        }
+    } catch (err) {
+        res.json(err)
+    }
 }
 // like or unlike a post
 async function likePost(req, res) {
