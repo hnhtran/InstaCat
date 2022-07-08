3afd3cff73beaa2dfe20795c3c4fccc15d6c6868
hnhtran <tran_eetas@yahoo.com>
2022-07-08 23:47:50 +1000

    Deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path')
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
@@ -19,7 +19,7 @@ app.use(logger('dev'))
 app.use(express.json())
 // Configure both serve-favicon & static
 // middleware to server from the 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 // Put API routes here, before the "catch all" route
