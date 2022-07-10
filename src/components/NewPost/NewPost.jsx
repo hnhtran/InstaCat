8cf892604af41c8c0de53c300ce6edc7aaea6f85
hnhtran <tran_eetas@yahoo.com>
2022-07-10 14:58:44 +1000

    new post created page update

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,53 +1,43 @@
 import { AddAPhoto } from "@mui/icons-material/";
 import "./NewPost.css";
 import FileBase64 from "react-file-base64";
-import { useParams } from "react-router-dom"
-import { useState } from "react";
-import { createPost } from '../../utilities/posts-api'
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { createPost } from "../../utilities/posts-api";
 
 export default function NewPost({ user, post, setPost }) {
-  let { userId } = useParams()
-  // console.log(user)
+  let { userId } = useParams();
+  const [isNewPostCreated, setIsNewPostCreated] = useState(false)
   const [postData, setPostData] = useState({
     user: user._id,
     likes: 0,
     description: "",
     image: "",
-  })
+  });
 
   const handleChange = (event) => {
     setPostData({
       ...postData,
       [event.target.name]: event.target.value,
     });
-  }
-  //   const handleImageChange = (event) => {
-  // 	setPostData({
-  // 		...postData,
-  // 		image: event.base64,
-  // 	});
-  // }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log(postData)
-    const newPost = await createPost(postData)
-    // console.log(post)
-    setPost(newPost)
-  }
+    const newPost = await createPost(postData);
+    setPost(newPost);
+    setIsNewPostCreated(true)
+    setPostData({
+      user: user._id,
+      likes: 0,
+      description: "",
+      image: "",
+    })
+  };
 
-  // const [image, setImage] = useState(null);
-  // const [imageUrl, setImageUrl] = useState(null);
-  // const onImageChange = (e) => {
-  // 	if(e.target.files && e.target.files[0]){
-  // 		setImage(e.target.files[0]);
-  // 	}
-  // 	// setImage(e.target.files[0]);
-  // 	// setImageUrl(URL.createObjectURL(e.target.files[0]));
-  // 	setPostData({...postData, image: image})
-  // 	// console.log(imageUrl)
-  // 	// console.log(image)
-  // }
+  // useEffect(() => {
+  //   isNewPostCreated && setIsNewPostCreated(false)
+  // }, [post])
 
   return (
     <>
@@ -55,12 +45,16 @@ export default function NewPost({ user, post, setPost }) {
         <form autoComplete="off">
           <div className="newPostsContainer">
             <div className="newPostsTop">
-              <img className="profilePicture" src={user.avatar} alt="Add Photo" />
+              <img
+                className="profilePicture"
+                src={user.avatar}
+                alt="Add Photo"
+              />
               <input
                 placeholder="Show us your cats!"
                 className="newPostInput"
-                type='text'
-                name='description'
+                type="text"
+                name="description"
                 value={postData.description}
                 onChange={handleChange}
                 required
@@ -75,18 +69,34 @@ export default function NewPost({ user, post, setPost }) {
                   <FileBase64
                     type="file"
                     multiple={false}
-                    name='image'
-                    onDone={({ base64 }) => setPostData({ ...postData, image: base64 })}
+                    name="image"
+                    onDone={({ base64 }) =>
+                      setPostData({ ...postData, image: base64 })
+                    }
                   />
-                  {/* <input type='file' onChange={onImageChange}/> */}
                 </div>
               </div>
-              <button className="addPost" type="submit">Add post</button>
+              <button className="addPost" type="submit">
+                Add post
+              </button>
             </div>
           </div>
         </form>
       </div>
-      <div>
+      <h1>
+      {isNewPostCreated ? 'post created' : 'no new post created yet'}
+      {/* {setIsNewPostCreated(false)} */}
+          {console.log(isNewPostCreated)}
+      </h1>
+      <div className="post">
+        <div className="postWrapper">
+          <div className='postText'>
+            <span className='postCreated'>{!postData.description ? 'About?' : postData.description}</span>
+               <hr />
+              <br />
+            <img alt="your cat's image here" src={postData.image} />
+          </div>
+        </div>
       </div>
     </>
   );
