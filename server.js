c7c60656a2c6cf82fc5094b67b7827977fe1ea17
hnhtran <tran_eetas@yahoo.com>
2022-12-07 19:38:00 +1100

    edit for render deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(logger('dev'))
 // body parser middleware - adds properties to req.body
 app.use(express.json())
 // middleware to server from the 'build' folder
-app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.static(path.resolve(__dirname, 'client/build')))
 
 // Put API routes here, before the "catch all" route
 // any routes leads to user now is shorten by '/' only in users router
@@ -31,7 +31,7 @@ app.use('/api/posts', require('./routes/api/posts'))
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
 app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 // app.use('/', require('./routes/api/posts'))
 
